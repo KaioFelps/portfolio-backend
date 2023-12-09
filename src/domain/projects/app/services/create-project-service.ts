dbc1f0b1bf5645d2f4fe53558464bf541581ac85
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-12-09 12:20:59 -0300

    fix: create project service

--- src/domain/projects/app/services/create-project-service.ts
+++ src/domain/projects/app/services/create-project-service.ts
@@ -2,11 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { IProjectsRepository } from '../repositories/projects-repository';
 import { Project } from '../entities/project';
 
+interface CreateProjectServiceRequest {
+  title: string;
+  topstory: string;
+  tags: string[];
+  links: string[];
+}
+
+interface CreateProjectServiceResponse {}
+
 @Injectable()
 export class CreateProjectService {
   constructor(private projectsRepository: IProjectsRepository) {}
 
-  async exec(project: Project) {
+  async exec({
+    links,
+    tags,
+    title,
+    topstory,
+  }: CreateProjectServiceRequest): Promise<CreateProjectServiceResponse> {
+    const project = Project.create({
+      links,
+      tags,
+      title,
+      topstory,
+    });
+
     await this.projectsRepository.create(project);
+
+    return {};
   }
 }
