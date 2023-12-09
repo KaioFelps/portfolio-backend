a26419510778f551ab69ff7985c86b816afcef5e
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-12-09 12:10:05 -0300

    chore: replace jest by vitest

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,21 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    // run this file before the tests begin
+    setupFiles: ['./test/setup-e2e.ts'],
+  },
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+    tsConfigPaths(),
+  ],
+});
