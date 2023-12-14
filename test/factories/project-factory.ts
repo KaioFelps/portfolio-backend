b47adcba9747a030bda36d8380d09571ee14baad
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-12-14 02:39:30 -0300

    test: project factory

---
+++ test/factories/project-factory.ts
@@ -0,0 +1,21 @@
+import { Optional } from '@/core/types/optional';
+import { Project, ProjectProps } from '@/domain/projects/app/entities/project';
+import { faker } from '@faker-js/faker';
+
+export class ProjectFactory {
+  static exec(
+    override?: Optional<
+      ProjectProps,
+      'createdAt' | 'links' | 'tags' | 'title' | 'topstory'
+    >,
+  ) {
+    return Project.create({
+      links: [faker.internet.url()],
+      tags: faker.word.words(3).split(' '),
+      title: faker.lorem.lines(1),
+      topstory: '',
+      createdAt: new Date(),
+      ...override,
+    });
+  }
+}
