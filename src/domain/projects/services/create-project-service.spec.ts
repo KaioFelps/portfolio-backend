3f4189e435a44ff7a4cc7c59184d84328c033fbe
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-12-16 22:04:27 -0300

    feat: mandatory user id to create new project

--- src/domain/projects/services/create-project-service.spec.ts
+++ src/domain/projects/services/create-project-service.spec.ts
@@ -1,21 +1,29 @@
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository';
 import { CreateProjectService } from './create-project-service';
 import { InMemoryProjectsRepository } from 'test/repositories/in-memory-projects-repository';
+import { UserFactory } from 'test/factories/user-factory';
 
 describe('Create Project Service', () => {
   let sut: CreateProjectService;
   let projectsRepository: InMemoryProjectsRepository;
+  let usersRepository: InMemoryUsersRepository;
 
   beforeEach(async () => {
     projectsRepository = new InMemoryProjectsRepository();
-    sut = new CreateProjectService(projectsRepository);
+    usersRepository = new InMemoryUsersRepository();
+    sut = new CreateProjectService(projectsRepository, usersRepository);
   });
 
   it('should create a project', async () => {
+    const user = UserFactory.exec('admin');
+    usersRepository.items.push(user);
+
     const result = await sut.exec({
       title: 'testing project',
       links: [],
       tags: ['back-end'],
       topstory: '',
+      userId: user.id.toValue(),
     });
 
     expect(result.isOk()).toBe(true);
--- src/domain/projects/services/create-project-service.ts
+++ src/domain/projects/services/create-project-service.ts
@@ -1,27 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { IProjectsRepository } from '../repositories/projects-repository';
 import { Project } from '../entities/project';
-import { Either, ok } from '@/core/types/either';
+import { Either, fail, ok } from '@/core/types/either';
+import { IUsersRepository } from '@/domain/users/repositories/users-repository';
+import { UnauthorizedError } from '@/core/errors/unauthorized-error';
 
 interface CreateProjectServiceRequest {
+  userId: string;
   title: string;
   topstory: string;
   tags: string[];
   links: string[];
 }
 
-type CreateProjectServiceResponse = Either<null, Project>;
+type CreateProjectServiceResponse = Either<
+  UnauthorizedError,
+  { project: Project }
+>;
 
 @Injectable()
 export class CreateProjectService {
-  constructor(private projectsRepository: IProjectsRepository) {}
+  constructor(
+    private projectsRepository: IProjectsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
 
   async exec({
+    userId,
     links,
     tags,
     title,
     topstory,
   }: CreateProjectServiceRequest): Promise<CreateProjectServiceResponse> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      return fail(new UnauthorizedError());
+    }
+
     const project = Project.create({
       links,
       tags,
@@ -31,6 +47,6 @@ export class CreateProjectService {
 
     await this.projectsRepository.create(project);
 
-    return ok(project);
+    return ok({ project });
   }
 }
