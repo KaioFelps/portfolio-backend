afc2aa5f06fa33293e66f91d6f26b76522383a15
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-19 15:55:21 -0300

    feat: project tag now extends tag entity

--- src/domain/projects/entities/project-tag.ts
+++ src/domain/projects/entities/project-tag.ts
@@ -2,29 +2,25 @@ import { Entity } from '@/core/entities/entity';
 import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 
 export interface ProjectTagProps {
-  value: string;
+  tagId: EntityUniqueId;
   projectId: EntityUniqueId;
 }
 
 export class ProjectTag extends Entity<ProjectTagProps> {
-  constructor(props: ProjectTagProps, id?: EntityUniqueId) {
+  private constructor(props: ProjectTagProps, id?: EntityUniqueId) {
     super(props, id);
   }
 
   static create(props: ProjectTagProps, id?: EntityUniqueId) {
-    const tag = new ProjectTag({ ...props }, id);
-    return tag;
+    const projectTag = new ProjectTag({ ...props }, id);
+    return projectTag;
   }
 
-  get value() {
-    return this.props.value;
+  get tagId() {
+    return this.props.tagId;
   }
 
   get projectId() {
-    return this.projectId;
-  }
-
-  set value(value: string) {
-    this.props.value = value;
+    return this.props.projectId;
   }
 }
