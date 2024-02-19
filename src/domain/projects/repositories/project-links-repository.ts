defac68b6d89bc6152fc4e421149442e6cc13c39
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-19 03:00:13 -0300

    feat: project links repository

--- src/domain/projects/repositories/project-links-repository.ts
+++ src/domain/projects/repositories/project-links-repository.ts
@@ -1,6 +1,10 @@
+import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 import { ProjectLink } from '../entities/project-link';
 
 export abstract class IProjectLinksRepository {
   abstract createMany(links: ProjectLink[]): Promise<void>;
   abstract deleteMany(links: ProjectLink[]): Promise<void>;
+  abstract findManyByProjectId(
+    projectId: EntityUniqueId,
+  ): Promise<ProjectLink[]>;
 }
