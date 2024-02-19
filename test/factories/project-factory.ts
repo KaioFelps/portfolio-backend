e54bcba90de952dbffa40aaaec055f044e4cbbad
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-19 16:18:07 -0300

    fix: project factory

--- test/factories/project-factory.ts
+++ test/factories/project-factory.ts
@@ -1,11 +1,9 @@
 import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 import { Optional } from '@/core/types/optional';
 import { Project, ProjectProps } from '@/domain/projects/entities/project';
-import { ProjectLink } from '@/domain/projects/entities/project-link';
 import { ProjectLinkList } from '@/domain/projects/entities/project-link-list';
 import { ProjectTagList } from '@/domain/projects/entities/project-tag-link';
 import { faker } from '@faker-js/faker';
-import { randomUUID } from 'crypto';
 
 export class ProjectFactory {
   static exec(
@@ -15,20 +13,16 @@ export class ProjectFactory {
     >,
     id?: EntityUniqueId,
   ) {
-    const projectId = id ?? new EntityUniqueId(randomUUID());
-
     return Project.create(
       {
-        links: new ProjectLinkList([
-          ProjectLink.create({ value: faker.internet.url(), projectId }),
-        ]),
+        links: new ProjectLinkList(),
         tags: new ProjectTagList(),
         title: faker.lorem.lines(1),
         topstory: '',
         createdAt: new Date(),
         ...override,
       },
-      projectId,
+      id,
     );
   }
 }
