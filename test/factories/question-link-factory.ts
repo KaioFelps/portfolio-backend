de489c50bb41658820da5dac2d88dc6bacfcb76d
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-19 15:55:46 -0300

    feat: question link factory

---
+++ test/factories/question-link-factory.ts
@@ -0,0 +1,19 @@
+import { EntityUniqueId } from '@/core/entities/entity-unique-id';
+import { Optional } from '@/core/types/optional';
+import {
+  ProjectLink,
+  ProjectLinkProps,
+} from '@/domain/projects/entities/project-link';
+import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProjectLinkFactory {
+  static exec(override?: Optional<ProjectLinkProps, 'projectId' | 'value'>) {
+    return ProjectLink.create({
+      projectId: new EntityUniqueId(),
+      value: faker.internet.url(),
+      ...override,
+    });
+  }
+}
