93c69fe619de7d649e83e812746c5c1ef33d2628
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-19 03:00:19 -0300

    feat: project tags repository

--- src/domain/projects/repositories/project-tags-repository.ts
+++ src/domain/projects/repositories/project-tags-repository.ts
@@ -1,6 +1,10 @@
+import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 import { ProjectTag } from '@/domain/projects/entities/project-tag';
 
-export abstract class PrismaProjectTagsRepository {
+export abstract class IProjectTagsRepository {
   abstract createMany(tags: ProjectTag[]): Promise<void>;
   abstract deleteMany(tags: ProjectTag[]): Promise<void>;
+  abstract findManyByProjectId(
+    projectId: EntityUniqueId,
+  ): Promise<ProjectTag[]>;
 }
