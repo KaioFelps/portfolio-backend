f597b322d8bca2d6b4dd9e0d265b219a3d7fa2a6
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-19 15:55:39 -0300

    fix: project factory

--- test/factories/project-factory.ts
+++ test/factories/project-factory.ts
@@ -1,6 +1,11 @@
+import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 import { Optional } from '@/core/types/optional';
 import { Project, ProjectProps } from '@/domain/projects/entities/project';
+import { ProjectLink } from '@/domain/projects/entities/project-link';
+import { ProjectLinkList } from '@/domain/projects/entities/project-link-list';
+import { ProjectTagList } from '@/domain/projects/entities/project-tag-link';
 import { faker } from '@faker-js/faker';
+import { randomUUID } from 'crypto';
 
 export class ProjectFactory {
   static exec(
@@ -8,14 +13,22 @@ export class ProjectFactory {
       ProjectProps,
       'createdAt' | 'links' | 'tags' | 'title' | 'topstory'
     >,
+    id?: EntityUniqueId,
   ) {
-    return Project.create({
-      links: [faker.internet.url()],
-      tags: faker.word.words(3).split(' '),
-      title: faker.lorem.lines(1),
-      topstory: '',
-      createdAt: new Date(),
-      ...override,
-    });
+    const projectId = id ?? new EntityUniqueId(randomUUID());
+
+    return Project.create(
+      {
+        links: new ProjectLinkList([
+          ProjectLink.create({ value: faker.internet.url(), projectId }),
+        ]),
+        tags: new ProjectTagList(),
+        title: faker.lorem.lines(1),
+        topstory: '',
+        createdAt: new Date(),
+        ...override,
+      },
+      projectId,
+    );
   }
 }
