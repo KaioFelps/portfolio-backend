b373a352eeec0895f3a7ee7dd6c7c350769ee2bc
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-20 02:43:20 -0300

    test: delete project service

--- src/domain/projects/services/delete-project-service.spec.ts
+++ src/domain/projects/services/delete-project-service.spec.ts
@@ -1,24 +1,42 @@
-import { Project } from '../entities/project';
 import { InMemoryProjectsRepository } from 'test/repositories/in-memory-projects-repository';
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
 import { DeleteProjectService } from './delete-project-service';
+import { InMemoryProjectLinksRepository } from 'test/repositories/in-memory-project-links-repository';
+import { InMemoryProjectTagsRepository } from 'test/repositories/in-memory-project-tags-repository';
+import { ProjectLinkList } from '../entities/project-link-list';
+import { ProjectTagList } from '../entities/project-tag-link';
+import { ProjectTagFactory } from 'test/factories/project-tag-factory';
+import { ProjectFactory } from 'test/factories/project-factory';
 
 describe('Delete Project Service', () => {
   let sut: DeleteProjectService;
   let projectsRepository: InMemoryProjectsRepository;
+  let projectLinksRepository: InMemoryProjectLinksRepository;
+  let projectTagsRepository: InMemoryProjectTagsRepository;
 
   beforeEach(async () => {
-    projectsRepository = new InMemoryProjectsRepository();
+    projectLinksRepository = new InMemoryProjectLinksRepository();
+
+    projectTagsRepository = new InMemoryProjectTagsRepository();
+
+    projectsRepository = new InMemoryProjectsRepository(
+      projectTagsRepository,
+      projectLinksRepository,
+    );
+
     sut = new DeleteProjectService(projectsRepository);
   });
 
   it('should delete a project if exists', async () => {
-    const project = Project.create({
+    const project = ProjectFactory.exec({
       title: 'testing project',
-      links: [],
-      tags: ['back-end'],
+      links: new ProjectLinkList(),
       topstory: '',
     });
+
+    project.tags = new ProjectTagList([
+      ProjectTagFactory.exec({ value: 'back-end', projectId: project.id }),
+    ]);
 
     projectsRepository.items.push(project);
 
