18d08c6f7ed3d827eae880302e40e6868cefc18e
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-20 02:40:29 -0300

    chore: delete raw tag related stuff

--- src/core/repositories/tags-repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityUniqueId } from '../entities/entity-unique-id';
-import { Tag } from '../entities/tag';
-
-export abstract class ITagsRepository {
-  abstract findManyByValue(value: string): Promise<Tag[]>;
-  abstract findById(id: EntityUniqueId): Promise<Tag | null>;
-}
