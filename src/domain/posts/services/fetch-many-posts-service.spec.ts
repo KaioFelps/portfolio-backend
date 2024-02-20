efce0fd490e411dc94a0bb1398eedc365008b359
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-20 03:21:28 -0300

    test: fetch many posts service

--- src/domain/posts/services/fetch-many-posts-service.spec.ts
+++ src/domain/posts/services/fetch-many-posts-service.spec.ts
@@ -1,6 +1,8 @@
 import { InMemoryPostsRepository } from 'test/repositories/in-memory-posts-repository';
 import { PostFactory } from 'test/factories/post-factory';
 import { FetchManyPostsService } from './fetch-many-posts-service';
+import { PostTagList } from '../entities/post-tag-list';
+import { PostTagFactory } from 'test/factories/post-tag-factory';
 
 describe('Fetch Many Posts Service', () => {
   let sut: FetchManyPostsService;
@@ -12,21 +14,25 @@ describe('Fetch Many Posts Service', () => {
   });
 
   it('should fetch posts that corresponds to the params', async () => {
-    postsRepository.items.push(
-      PostFactory.exec({ tags: [], title: 'design de fs' }),
-    );
-    postsRepository.items.push(
-      PostFactory.exec({ tags: ['design', 'back-end'], title: 'teste 1' }),
-    );
-    postsRepository.items.push(
-      PostFactory.exec({ tags: [], title: 'teste 2' }),
-    );
-    postsRepository.items.push(
-      PostFactory.exec({ tags: [], title: 'teste 3' }),
-    );
-    postsRepository.items.push(
-      PostFactory.exec({ tags: [], title: 'teste 4' }),
-    );
+    postsRepository.items.push(PostFactory.exec({ title: 'design de fs' }));
+
+    const postToBePushed = PostFactory.exec({ title: 'teste 1' });
+    postToBePushed.tags = new PostTagList([
+      PostTagFactory.exec({
+        value: 'design',
+        postId: postToBePushed.id,
+      }),
+      PostTagFactory.exec({
+        value: 'back-end',
+        postId: postToBePushed.id,
+      }),
+    ]);
+
+    postsRepository.items.push(postToBePushed);
+
+    postsRepository.items.push(PostFactory.exec({ title: 'teste 2' }));
+    postsRepository.items.push(PostFactory.exec({ title: 'teste 3' }));
+    postsRepository.items.push(PostFactory.exec({ title: 'teste 4' }));
 
     let result = await sut.exec({
       page: 2,
