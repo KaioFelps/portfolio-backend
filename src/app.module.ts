0a00bfe2b45848a1c25678e45f3084df8ca0c830
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-02-21 04:38:16 -0300

    feat: app module - config import - update imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infra/db/database.module';
+import { AuthModule } from './infra/auth/auth.module';
+import { EnvModule } from './infra/env/env.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './infra/env/env';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuthModule,
+    EnvModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
