95a06c9d18c8c9052c25c0f238f5854178265ad7
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-10 19:34:59 -0300

    feat: users providers to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,29 @@ import { AuthController } from './controllers/auth.controller';
 import { AuthenticateService } from '@/domain/users/services/authenticate-service';
 import { CryptographyModule } from '../crypt/cryptography.module';
 import { DatabaseModule } from '../db/database.module';
+import { CreateUserService } from '@/domain/users/services/create-user-service';
+import { DeleteUserService } from '@/domain/users/services/delete-user-service';
+import { EditUserService } from '@/domain/users/services/edit-user-service';
+import { RefreshAuthenticationService } from '@/domain/users/services/refresh-authentication-service';
+import { FetchManyUsersService } from '@/domain/users/services/fetch-many-users-service';
 
 @Module({
   imports: [CryptographyModule, DatabaseModule],
   controllers: [AuthController],
-  providers: [AuthenticateService],
+  providers: [
+    // logs ///////////////////////////
+
+    // posts ///////////////////////////
+
+    // projects ///////////////////////////
+
+    // users ///////////////////////////
+    AuthenticateService,
+    CreateUserService,
+    DeleteUserService,
+    EditUserService,
+    RefreshAuthenticationService,
+    FetchManyUsersService,
+  ],
 })
 export class HttpModule {}
