60ab9c8ea9bc842901034934668ec8f63f7fc6c8
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-10 19:24:06 -0300

    chore: env - new required fields on .env fiel

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -5,6 +5,8 @@ export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3000),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
+  NODE_ENV: z.string().optional().default('development'),
+  DOMAIN: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
