d4696ec58873694684f341a8b6c00ba214708471
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-29 19:59:52 -0300

    feat: delete project service - now only admin can delete a project - test

--- src/domain/projects/services/delete-project-service.spec.ts
+++ src/domain/projects/services/delete-project-service.spec.ts
@@ -7,12 +7,16 @@ import { ProjectLinkList } from '../entities/project-link-list';
 import { ProjectTagList } from '../entities/project-tag-link';
 import { ProjectTagFactory } from 'test/factories/project-tag-factory';
 import { ProjectFactory } from 'test/factories/project-factory';
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository';
+import { UserFactory } from 'test/factories/user-factory';
+import { UnauthorizedError } from '@/core/errors/unauthorized-error';
 
 describe('Delete Project Service', () => {
   let sut: DeleteProjectService;
   let projectsRepository: InMemoryProjectsRepository;
   let projectLinksRepository: InMemoryProjectLinksRepository;
   let projectTagsRepository: InMemoryProjectTagsRepository;
+  let usersRepository: InMemoryUsersRepository;
 
   beforeEach(async () => {
     projectLinksRepository = new InMemoryProjectLinksRepository();
@@ -24,7 +28,9 @@ describe('Delete Project Service', () => {
       projectLinksRepository,
     );
 
-    sut = new DeleteProjectService(projectsRepository);
+    usersRepository = new InMemoryUsersRepository();
+
+    sut = new DeleteProjectService(projectsRepository, usersRepository);
   });
 
   it('should delete a project if exists', async () => {
@@ -40,8 +46,12 @@ describe('Delete Project Service', () => {
 
     projectsRepository.items.push(project);
 
+    const user = UserFactory.exec('admin');
+    usersRepository.items.push(user);
+
     const result = await sut.exec({
       projectId: project.id.toValue(),
+      userId: user.id.toValue(),
     });
 
     expect(result.isOk()).toBe(true);
@@ -49,13 +59,43 @@ describe('Delete Project Service', () => {
     expect(projectsRepository.items.length).toBe(0);
   });
 
-  it("shouldn't delete a question that doesn't exist", async () => {
+  it("shouldn't delete a project that doesn't exist", async () => {
+    const user = UserFactory.exec('admin');
+    usersRepository.items.push(user);
+
     const result = await sut.exec({
       projectId: 'vsd',
+      userId: user.id.toValue(),
     });
 
     expect(result.isFail()).toBe(true);
 
     expect(result.value).toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("shouldn't let a non-admin user deledte a project.", async () => {
+    const project = ProjectFactory.exec({
+      title: 'testing project',
+      links: new ProjectLinkList(),
+      topstory: '',
+    });
+
+    project.tags = new ProjectTagList([
+      ProjectTagFactory.exec({ value: 'back-end', projectId: project.id }),
+    ]);
+
+    projectsRepository.items.push(project);
+
+    const user = UserFactory.exec('editor');
+    usersRepository.items.push(user);
+
+    const result = await sut.exec({
+      projectId: project.id.toValue(),
+      userId: user.id.toValue(),
+    });
+
+    expect(result.isFail()).toBe(true);
+    expect(result.value).toBeInstanceOf(UnauthorizedError);
+    expect(projectsRepository.items.length).toBe(1);
   });
 });
--- src/domain/projects/services/delete-project-service.ts
+++ src/domain/projects/services/delete-project-service.ts
@@ -2,24 +2,39 @@ import { Injectable } from '@nestjs/common';
 import { IProjectsRepository } from '../repositories/projects-repository';
 import { Either, fail, ok } from '@/core/types/either';
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import { IUsersRepository } from '@/domain/users/repositories/users-repository';
+import { BadRequestError } from '@/core/errors/bad-request-error';
+import { UnauthorizedError } from '@/core/errors/unauthorized-error';
+import { UserRole } from '@/domain/users/entities/user';
 
 interface DeleteProjectServiceRequest {
+  userId: string;
   projectId: string;
-  title?: string;
-  topstory?: string;
-  tags?: string[];
-  links?: string[];
 }
 
 type DeleteProjectServiceResponse = Either<ResourceNotFoundError, object>;
 
 @Injectable()
 export class DeleteProjectService {
-  constructor(private projectsRepository: IProjectsRepository) {}
+  constructor(
+    private projectsRepository: IProjectsRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
 
   async exec({
+    userId,
     projectId,
   }: DeleteProjectServiceRequest): Promise<DeleteProjectServiceResponse> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      return fail(new BadRequestError());
+    }
+
+    if (user.role !== UserRole.admin) {
+      return fail(new UnauthorizedError());
+    }
+
     const project = await this.projectsRepository.findById(projectId);
 
     if (!project) {
