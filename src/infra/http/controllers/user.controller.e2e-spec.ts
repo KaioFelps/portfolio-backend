466baa3376d7acf8c8b35067f5cbda092f2ce30b
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-29 19:31:06 -0300

    test: user controller

--- src/infra/http/controllers/user.controller.e2e-spec.ts
+++ src/infra/http/controllers/user.controller.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { AppModule } from '@/app.module';
 import { TokenPayload } from '@/infra/auth/jwt-strategy';
 import { DatabaseModule } from '@/infra/db/database.module';
+import { PrismaService } from '@/infra/db/prisma/prisma-service';
 import { INestApplication } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
@@ -12,6 +13,7 @@ describe('UserController', () => {
   let app: INestApplication;
   let userFactory: UserFactory;
   let jwt: JwtService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,6 +24,7 @@ describe('UserController', () => {
     app = module.createNestApplication();
     userFactory = module.get(UserFactory);
     jwt = module.get(JwtService);
+    prisma = module.get(PrismaService);
 
     await app.init();
   });
@@ -58,5 +61,64 @@ describe('UserController', () => {
       role: 'EDITOR',
       createdAt: expect.any(String),
     });
+  });
+
+  test('[PUT] /user/:id/edit', async () => {
+    const adminUser = await userFactory.createAndPersist('admin', {
+      name: 'Kaio',
+      email: 'kaio2@gmail.com',
+      password: await hash('123456', 6),
+    });
+
+    const token = await jwt.signAsync({
+      name: adminUser.name,
+      role: adminUser.role,
+      sub: adminUser.id.toValue(),
+    });
+
+    const anotherUser = await userFactory.createAndPersist('editor');
+
+    await supertest(app.getHttpServer())
+      .put(`/user/${anotherUser.id.toValue()}/edit`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        name: 'Nome editado',
+        role: 'ADMIN',
+      })
+      .expect(204);
+
+    const userOnDb = await prisma.user.findFirst({
+      where: {
+        id: anotherUser.id.toValue(),
+      },
+    });
+
+    expect(userOnDb?.name).toEqual('Nome editado');
+    expect(userOnDb?.role).toEqual('ADMIN');
+  });
+
+  test('[DELETE] user/:id/delete', async () => {
+    const adminUser = await userFactory.createAndPersist('admin');
+    const anotherUser = await userFactory.createAndPersist('editor');
+
+    const token = await jwt.signAsync({
+      name: adminUser.name,
+      role: adminUser.role,
+      sub: adminUser.id.toValue(),
+    });
+
+    await supertest(app.getHttpServer())
+      .delete(`/user/${anotherUser.id.toValue()}/delete`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send()
+      .expect(204);
+
+    const userOnDb = await prisma.user.findUnique({
+      where: {
+        id: anotherUser.id.toValue(),
+      },
+    });
+
+    expect(userOnDb).toBeNull();
   });
 });
