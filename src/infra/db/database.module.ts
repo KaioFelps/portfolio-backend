c462e84daf96fd9401fee6e06a8b9d8c5e63f25f
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-03-29 20:59:42 -0300

    feat: database module

--- src/infra/db/database.module.ts
+++ src/infra/db/database.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma-service';
 import { IUsersRepository } from '@/domain/users/repositories/users-repository';
 import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository';
+import { IProjectsRepository } from '@/domain/projects/repositories/projects-repository';
+import { PrismaProjectsRepository } from './prisma/repositories/prisma-projects-repository';
+import { PrismaProjectLinksRepository } from './prisma/repositories/prisma-project-links-repository';
+import { IProjectLinksRepository } from '@/domain/projects/repositories/project-links-repository';
+import { PrismaProjectTagsRepository } from './prisma/repositories/prisma-project-tag-repository';
+import { IProjectTagsRepository } from '@/domain/projects/repositories/project-tags-repository';
 
 @Module({
   providers: [
@@ -10,7 +16,25 @@ import { PrismaUsersRepository } from './prisma/repositories/prisma-users-reposi
       provide: IUsersRepository,
       useClass: PrismaUsersRepository,
     },
+    {
+      provide: IProjectsRepository,
+      useClass: PrismaProjectsRepository,
+    },
+    {
+      provide: IProjectLinksRepository,
+      useClass: PrismaProjectLinksRepository,
+    },
+    {
+      provide: IProjectTagsRepository,
+      useClass: PrismaProjectTagsRepository,
+    },
   ],
-  exports: [PrismaService, IUsersRepository],
+  exports: [
+    PrismaService,
+    IUsersRepository,
+    IProjectsRepository,
+    IProjectLinksRepository,
+    IProjectTagsRepository,
+  ],
 })
 export class DatabaseModule {}
