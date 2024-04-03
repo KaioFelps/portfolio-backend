db66d79009f342b01ada4d5929ff337d89c70d47
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-02 22:19:50 -0300

    feat: project factory - create and persist method

--- test/factories/project-factory.ts
+++ test/factories/project-factory.ts
@@ -3,9 +3,15 @@ import { Optional } from '@/core/types/optional';
 import { Project, ProjectProps } from '@/domain/projects/entities/project';
 import { ProjectLinkList } from '@/domain/projects/entities/project-link-list';
 import { ProjectTagList } from '@/domain/projects/entities/project-tag-link';
+import { PrismaProjectMapper } from '@/infra/db/prisma/mappers/prisma-project-mapper';
+import { PrismaService } from '@/infra/db/prisma/prisma-service';
 import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class ProjectFactory {
+  constructor(private prisma: PrismaService) {}
+
   static exec(
     override?: Optional<
       ProjectProps,
@@ -24,5 +30,19 @@ export class ProjectFactory {
       },
       id,
     );
+  }
+
+  async createAndPersist(
+    override?: Optional<
+      ProjectProps,
+      'createdAt' | 'links' | 'tags' | 'title' | 'topstory'
+    >,
+  ) {
+    const project = ProjectFactory.exec(override);
+    const prismaUser = PrismaProjectMapper.toPrisma(project);
+
+    await this.prisma.project.create({ data: prismaUser });
+
+    return project;
   }
 }
