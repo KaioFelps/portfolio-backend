791f5fb8f0af162a10674878dafc4ff0103bc085
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-02 22:25:17 -0300

    feat: project controller + e2e tests

---
+++ src/infra/http/controllers/project.controller.e2e-spec.ts
@@ -0,0 +1,120 @@
+import { AppModule } from '@/app.module';
+import { DatabaseModule } from '@/infra/db/database.module';
+import { PrismaService } from '@/infra/db/prisma/prisma-service';
+import { INestApplication } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { Test, TestingModule } from '@nestjs/testing';
+import supertest from 'supertest';
+import { ProjectFactory } from 'test/factories/project-factory';
+import { UserFactory } from 'test/factories/user-factory';
+
+describe('ProjectController', () => {
+  let app: INestApplication;
+  let userFactory: UserFactory;
+  let projectFactory: ProjectFactory;
+  let jwt: JwtService;
+  let prisma: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory, ProjectFactory, JwtModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    userFactory = module.get(UserFactory);
+    projectFactory = module.get(ProjectFactory);
+    jwt = module.get(JwtService);
+    prisma = module.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /project/new', async () => {
+    const user = await userFactory.createAndPersist('admin');
+    const token = await jwt.signAsync({
+      name: user.name,
+      role: user.role,
+      sub: user.id.toValue(),
+    });
+
+    const result = await supertest(app.getHttpServer())
+      .post('/project/new')
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        title: 'Hidro Mourão',
+        topstory: 'https://i.imgur.com/payZTW9.png',
+        tags: ['front-end'],
+        links: ['hidromourao.com'],
+      })
+      .expect(201);
+
+    expect(result.body.project).toMatchObject({
+      id: expect.any(String),
+      title: 'Hidro Mourão',
+      tags: expect.arrayContaining([
+        expect.objectContaining({
+          value: 'front-end',
+        }),
+      ]),
+      links: expect.arrayContaining([
+        expect.objectContaining({
+          value: 'hidromourao.com',
+        }),
+      ]),
+      topstory: expect.any(String),
+      createdAt: expect.any(String),
+    });
+  });
+
+  test('[PUT] /project/:id/edit', async () => {
+    const user = await userFactory.createAndPersist('admin');
+    const token = await jwt.signAsync({
+      name: user.name,
+      role: user.role,
+      sub: user.id.toValue(),
+    });
+
+    const project = await projectFactory.createAndPersist();
+
+    await supertest(app.getHttpServer())
+      .put(`/project/${project.id.toValue()}/edit`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        title: 'Edited title of the project',
+        tags: ['front-end'],
+      })
+      .expect(204);
+
+    const projectOnDb = await prisma.project.findUnique({
+      where: { id: project.id.toValue() },
+      include: { tags: true },
+    });
+
+    expect(projectOnDb!.title).toEqual('Edited title of the project');
+    expect(projectOnDb!.tags).toEqual(
+      expect.arrayContaining([expect.objectContaining({ value: 'front-end' })]),
+    );
+  });
+
+  test('[DELETE] /project/:id/delete', async () => {
+    const user = await userFactory.createAndPersist('admin');
+    const token = await jwt.signAsync({
+      name: user.name,
+      role: user.role,
+      sub: user.id.toValue(),
+    });
+
+    const project = await projectFactory.createAndPersist();
+
+    await supertest(app.getHttpServer())
+      .delete(`/project/${project.id.toValue()}/delete`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send()
+      .expect(204);
+
+    const projectOnDb = await prisma.project.findMany();
+
+    expect(projectOnDb.length).toBe(0);
+  });
+});
---
+++ src/infra/http/controllers/project.controller.ts
@@ -0,0 +1,132 @@
+import { CreateProjectService } from '@/domain/projects/services/create-project-service';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateProjectDto } from '../dtos/create-project';
+import { CurrentUser } from '@/infra/auth/decorators/current-user';
+import { TokenPayload } from '@/infra/auth/jwt-strategy';
+import { UnauthorizedError } from '@/core/errors/unauthorized-error';
+import { UpdateProjectDto } from '../dtos/update-project';
+import { EditProjectService } from '@/domain/projects/services/edit-project-service';
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import { DeleteProjectService } from '@/domain/projects/services/delete-project-service';
+import { BadRequestError } from '@/core/errors/bad-request-error';
+import { ProjectPresenter } from '../presenters/project-presenter';
+
+/*
+import { ZodValidatorPipe } from '@/infra/lib/zod-validator-pipe';
+import { z } from 'zod';
+
+  // VALIDATION USING ZOD
+  const createProjectBody = z.object({
+    title: z.string(),
+    topstory: z.string(),
+    tags: z.string().array(),
+    links: z.string().array(),
+  });
+  const createProjectBodyPipe = new ZodValidatorPipe(createProjectBody);
+  type CreateProjectBody = z.infer<typeof createProjectBody>;
+
+  @Post("/")
+  async createProject(@Body(createProjectBodyPipe) body: CreateProjectBody) {}
+*/
+
+@Controller('project')
+export class ProjectController {
+  constructor(
+    private createProjectService: CreateProjectService,
+    private editProjectService: EditProjectService,
+    private deleteProjectService: DeleteProjectService,
+  ) {}
+
+  @Post('new')
+  @HttpCode(201)
+  async create(
+    @Body() createProjectDto: CreateProjectDto,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    const { links, tags, title, topstory } = createProjectDto;
+
+    const result = await this.createProjectService.exec({
+      links,
+      tags,
+      title,
+      topstory,
+      userId: user.sub,
+    });
+
+    if (result.isFail()) {
+      switch (result.value.constructor) {
+        case UnauthorizedError:
+          throw new UnauthorizedException();
+        default:
+          throw new BadRequestException();
+      }
+    }
+
+    return {
+      project: ProjectPresenter.toHTTP(result.value.project),
+    };
+  }
+
+  @Put('/:id/edit')
+  @HttpCode(204)
+  async update(
+    @Body() updateProjectDto: UpdateProjectDto,
+    @CurrentUser() user: TokenPayload,
+    @Param('id') projectId: string,
+  ) {
+    const result = await this.editProjectService.exec({
+      userId: user.sub,
+      projectId,
+      ...updateProjectDto,
+    });
+
+    if (result.isFail()) {
+      switch (result.value.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException(result.value.message);
+        default:
+          throw new BadRequestException();
+      }
+    }
+  }
+
+  @Delete('/:id/delete')
+  @HttpCode(204)
+  async delete(
+    @Param('id') projectId: string,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    const result = await this.deleteProjectService.exec({
+      projectId,
+      userId: user.sub,
+    });
+
+    if (result.isFail()) {
+      switch (result.value.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException();
+        case UnauthorizedError:
+          throw new UnauthorizedException();
+        case BadRequestError:
+          throw new BadRequestError();
+        default:
+          throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  // FETCH PROJECTS
+  // FETCHUSERS ON USERS CONTROLLER MISSING TOO
+}
