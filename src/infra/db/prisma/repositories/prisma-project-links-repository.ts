99ca216b5f41caa084d90b98d7fdec0161f11194
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-02 22:20:57 -0300

    feat: prisma project links repository - implement project links repository interface

--- src/infra/db/prisma/repositories/prisma-project-links-repository.ts
+++ src/infra/db/prisma/repositories/prisma-project-links-repository.ts
@@ -2,9 +2,11 @@ import { ProjectLink } from '@/domain/projects/entities/project-link';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma-service';
 import { PrismaProjectLinkMapper } from '../mappers/prisma-project-link-mapper';
+import { IProjectLinksRepository } from '@/domain/projects/repositories/project-links-repository';
+import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 
 @Injectable()
-export class PrismaProjectLinksRepository {
+export class PrismaProjectLinksRepository implements IProjectLinksRepository {
   constructor(private prisma: PrismaService) {}
 
   async createMany(links: ProjectLink[]): Promise<void> {
@@ -23,5 +25,17 @@ export class PrismaProjectLinksRepository {
         },
       },
     });
+  }
+
+  async findManyByProjectId(projectId: EntityUniqueId): Promise<ProjectLink[]> {
+    const prismaLinks = await this.prisma.link.findMany({
+      where: {
+        projectId: projectId.toValue(),
+      },
+    });
+
+    const links = prismaLinks.map(PrismaProjectLinkMapper.toDomain);
+
+    return links;
   }
 }
