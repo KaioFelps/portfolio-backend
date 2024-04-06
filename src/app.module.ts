a57f42032247d3f9f59ee2110d92e734d38663b7
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-06 17:16:50 -0300

    fix: global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthModule } from './infra/auth/auth.module';
 import { EnvModule } from './infra/env/env.module';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './infra/env/env';
 import { HttpModule } from './infra/http/http.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -16,6 +17,19 @@ import { HttpModule } from './infra/http/http.module';
     { module: EnvModule, global: true },
     HttpModule,
   ],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useFactory: () => {
+        return new ValidationPipe({
+          transform: true,
+          transformOptions: { enableImplicitConversion: true },
+          forbidNonWhitelisted: true,
+          stopAtFirstError: true,
+        });
+      },
+    },
+  ],
 })
 export class AppModule {}
