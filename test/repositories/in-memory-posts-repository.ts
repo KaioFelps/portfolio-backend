e545c95905afd43c82d4bcf0b9614ff5bd967b0b
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-21 20:21:45 -0300

    chore: in memory posts repository - implement latest posts repository interface

--- test/repositories/in-memory-posts-repository.ts
+++ test/repositories/in-memory-posts-repository.ts
@@ -1,10 +1,16 @@
 import { DomainEvents } from '@/core/events/domain-events';
 import { PaginationParams } from '@/core/types/pagination-params';
 import { Post } from '@/domain/posts/entities/post';
+import { PostWithAuthor } from '@/domain/posts/entities/value-objects/post-with-author';
 import { IPostsRepository } from '@/domain/posts/repositories/posts-repository';
+import { InMemoryUsersRepository } from './in-memory-users-repository';
+import { Slug } from '@/domain/posts/entities/value-objects/slug';
+import { PaginationResponse } from '@/core/types/pagination-responses';
 
 export class InMemoryPostsRepository implements IPostsRepository {
   public items: Post[] = [];
+
+  constructor(private usersRepository: InMemoryUsersRepository) {}
 
   async create(post: Post): Promise<void> {
     this.items.push(post);
@@ -24,7 +30,7 @@ export class InMemoryPostsRepository implements IPostsRepository {
     amount: itemsPerPage = 9,
     page = 1,
     query,
-  }: PaginationParams): Promise<Post[]> {
+  }: PaginationParams): Promise<PaginationResponse<Post>> {
     let posts: Post[] = [];
 
     if (query) {
@@ -51,9 +57,11 @@ export class InMemoryPostsRepository implements IPostsRepository {
       posts = this.items;
     }
 
+    const postsTotalCount = posts.length;
+
     posts = posts.slice((page - 1) * itemsPerPage, itemsPerPage * page);
 
-    return posts;
+    return { value: posts, totalCount: postsTotalCount };
   }
 
   async save(post: Post): Promise<void> {
@@ -68,5 +76,33 @@ export class InMemoryPostsRepository implements IPostsRepository {
     const itemIndex = this.items.findIndex((item) => item.id.equals(post.id));
 
     this.items.splice(itemIndex, 1);
+  }
+
+  async findBySlugWithAuthor(slug: string): Promise<PostWithAuthor | null> {
+    const post = this.items.find((item) => item.slug === slug);
+
+    if (!post) {
+      return null;
+    }
+
+    const author = await this.usersRepository.findById(post.authorId.toValue());
+
+    if (!author) {
+      throw new Error(
+        `Author with ID "${post.authorId.toString()} doesn't existe."`,
+      );
+    }
+
+    return PostWithAuthor.create({
+      author: author.name,
+      content: post.content,
+      createdAt: post.createdAt,
+      slug: Slug.create(post.slug),
+      tags: post.tags,
+      title: post.title,
+      topstory: post.topstory,
+      updatedAt: post.updatedAt,
+      id: post.id,
+    });
   }
 }
