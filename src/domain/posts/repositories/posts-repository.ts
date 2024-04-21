abd28a3db29284a17c22d6b6f09c7788fe7eb405
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-21 20:20:08 -0300

    feat: posts repository - add find by slug with author method - find many returns a PaginationResponse<Post> type object - implement it to `in-memory-posts-repository.ts`

--- src/domain/posts/repositories/posts-repository.ts
+++ src/domain/posts/repositories/posts-repository.ts
@@ -1,11 +1,17 @@
 import { PaginationParams } from '@/core/types/pagination-params';
 import { Post } from '../entities/post';
+import { PostWithAuthor } from '../entities/value-objects/post-with-author';
+import { PaginationResponse } from '@/core/types/pagination-responses';
 
 export abstract class IPostsRepository {
   abstract create(post: Post): Promise<void>;
   abstract findById(id: string): Promise<Post | null>;
   abstract findBySlug(slug: string): Promise<Post | null>;
-  abstract findMany(params: PaginationParams): Promise<Post[]>;
+  abstract findBySlugWithAuthor(slug: string): Promise<PostWithAuthor | null>;
+  abstract findMany(
+    params: PaginationParams,
+  ): Promise<PaginationResponse<Post>>;
+
   abstract save(post: Post): Promise<void>;
   abstract delete(post: Post): Promise<void>;
 }
