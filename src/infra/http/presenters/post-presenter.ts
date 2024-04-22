f940e4443639ca8d1e75d819271cd939ac649581
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-21 22:30:31 -0300

    chore: post presenter

--- src/infra/http/presenters/post-presenter.ts
+++ src/infra/http/presenters/post-presenter.ts
@@ -3,10 +3,10 @@ import { PostWithAuthor } from '@/domain/posts/entities/value-objects/post-with-
 export class PostPresenter {
   static toHTTP(post: PostWithAuthor) {
     return {
-      id: post.id,
+      id: post.id.toValue(),
       author: post.author,
       title: post.title,
-      slug: post.slug,
+      slug: post.slug.value,
       topstory: post.topstory,
       tags: post.tags,
       content: post.content,
