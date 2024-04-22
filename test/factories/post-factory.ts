35b9d5b2176f8354c7d239026dcc16b28bf6d33e
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-04-21 22:30:08 -0300

    chore: post factory - add createAndPersist method

--- test/factories/post-factory.ts
+++ test/factories/post-factory.ts
@@ -2,9 +2,15 @@ import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 import { Optional } from '@/core/types/optional';
 import { Post, PostProps } from '@/domain/posts/entities/post';
 import { PostTagList } from '@/domain/posts/entities/post-tag-list';
+import { PrismaPostMapper } from '@/infra/db/prisma/mappers/prisma-post-mapper';
+import { PrismaService } from '@/infra/db/prisma/prisma-service';
 import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class PostFactory {
+  constructor(private prisma: PrismaService) {}
+
   static exec(
     override?: Optional<
       PostProps,
@@ -27,5 +33,26 @@ export class PostFactory {
       createdAt: new Date(),
       ...override,
     });
+  }
+
+  async createAndPersist(
+    override?: Optional<
+      PostProps,
+      | 'createdAt'
+      | 'content'
+      | 'slug'
+      | 'tags'
+      | 'title'
+      | 'topstory'
+      | 'updatedAt'
+      | 'authorId'
+    >,
+  ) {
+    const post = PostFactory.exec(override);
+    const prismaPost = PrismaPostMapper.toPrisma(post);
+
+    await this.prisma.post.create({ data: prismaPost });
+
+    return post;
   }
 }
