73b706f9d3ed50e1c06c2fd43529a5b38ec3867a
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-09 23:37:53 -0300

    feat: prisma schema - map User table to users

---
+++ prisma/migrations/20240510023739_map_user_table_to_users/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `logs` DROP FOREIGN KEY `logs_dispatcher_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `posts` DROP FOREIGN KEY `posts_author_id_fkey`;
+
+-- DropTable
+DROP TABLE `User`;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'EDITOR') NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `posts` ADD CONSTRAINT `posts_author_id_fkey` FOREIGN KEY (`author_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `logs` ADD CONSTRAINT `logs_dispatcher_id_fkey` FOREIGN KEY (`dispatcher_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
