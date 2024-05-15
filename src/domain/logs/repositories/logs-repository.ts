600da51dc9024242426ebe93d99b1736b75ee741
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-15 13:32:53 -0300

    chore: logs repository - `findManyWithAuthor` interface - in memory `findManyWithAuthor` implementation

--- src/domain/logs/repositories/logs-repository.ts
+++ src/domain/logs/repositories/logs-repository.ts
@@ -1,7 +1,12 @@
 import { PaginationParams } from '@/core/types/pagination-params';
 import { Log } from '../entities/log';
+import { LogWithAuthor } from '../entities/value-objects/log-with-author';
+import { PaginationResponse } from '@/core/types/pagination-responses';
 
 export abstract class ILogsRepository {
   abstract create(log: Log): Promise<void>;
   abstract findMany(params: PaginationParams): Promise<Log[]>;
+  abstract findManyWithAuthor(
+    params: PaginationParams,
+  ): Promise<PaginationResponse<LogWithAuthor>>;
 }
--- test/repositories/in-memory-logs-repository.ts
+++ test/repositories/in-memory-logs-repository.ts
@@ -1,17 +1,23 @@
 import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 import { PaginationParams } from '@/core/types/pagination-params';
+import { PaginationResponse } from '@/core/types/pagination-responses';
 import { Log } from '@/domain/logs/entities/log';
+import { LogWithAuthor } from '@/domain/logs/entities/value-objects/log-with-author';
 import { ILogsRepository } from '@/domain/logs/repositories/logs-repository';
+import { InMemoryUsersRepository } from './in-memory-users-repository';
+import { QUANTITY_PER_PAGE } from '@/core/pagination-consts';
 
 export class InMemoryLogsRepository implements ILogsRepository {
   public items: Log[] = [];
+
+  constructor(private inMemoryUsersRepository: InMemoryUsersRepository) {}
 
   async create(log: Log): Promise<void> {
     this.items.push(log);
   }
 
   async findMany({
-    amount: itemsPerPage = 9,
+    amount: itemsPerPage = QUANTITY_PER_PAGE,
     page = 1,
     query,
   }: PaginationParams): Promise<Log[]> {
@@ -43,5 +49,63 @@ export class InMemoryLogsRepository implements ILogsRepository {
     logs = logs.slice((page - 1) * itemsPerPage, itemsPerPage * page);
 
     return logs;
+  }
+
+  async findManyWithAuthor({
+    query,
+    amount: itemsPerPage = QUANTITY_PER_PAGE,
+    page = 1,
+  }: PaginationParams): Promise<PaginationResponse<LogWithAuthor>> {
+    let logs: Log[] = [];
+
+    if (query) {
+      logs = this.items.filter((item) => {
+        if (item.action.includes(query.trim())) {
+          return item;
+        }
+
+        if (item.target === query) {
+          return item;
+        }
+
+        if (
+          item.dispatcherId &&
+          item.dispatcherId.equals(new EntityUniqueId(query))
+        ) {
+          return item;
+        }
+
+        return null;
+      });
+    } else {
+      logs = this.items;
+    }
+
+    logs = logs.slice((page - 1) * itemsPerPage, itemsPerPage * page);
+
+    const logsWithAuthor: LogWithAuthor[] = [];
+
+    logs.forEach(async (log) => {
+      logsWithAuthor.push(
+        LogWithAuthor.create({
+          action: log.action,
+          createdAt: log.createdAt,
+          target: log.target,
+          targetType: log.targetType,
+          dispatcher: log.dispatcherId
+            ? await this.inMemoryUsersRepository.findById(
+                log.dispatcherId.toValue(),
+              )
+            : null,
+        }),
+      );
+    });
+
+    const totalCount = logs.length;
+
+    return {
+      value: await logsWithAuthor,
+      totalCount,
+    };
   }
 }
