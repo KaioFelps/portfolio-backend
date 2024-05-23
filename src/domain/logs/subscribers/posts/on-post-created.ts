c7e29501aaad3cf4bd9bd43187220f8ef139cfbf
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-23 01:44:29 -0300

    feat: on post created - finishes it - add e2e test of it

--- src/domain/logs/subscribers/posts/on-post-created.ts
+++ src/domain/logs/subscribers/posts/on-post-created.ts
@@ -1,9 +1,11 @@
 import { EventHandler } from '@/core/events/event-handler';
 import { CreateLogService } from '../../services/create-log-service';
-import { LogAction } from '../../entities/log';
+import { LogAction, LogTargetType } from '../../entities/log';
 import { DomainEvents } from '@/core/events/domain-events';
 import { PostCreatedEvent } from '@/domain/posts/events/post-created-event';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class OnPostCreated implements EventHandler {
   constructor(private createLogService: CreateLogService) {
     this.setupSubscriptions();
@@ -19,6 +21,7 @@ export class OnPostCreated implements EventHandler {
   private async createLog({ post, occurredAt }: PostCreatedEvent) {
     await this.createLogService.exec({
       action: LogAction.created,
+      targetType: LogTargetType.post,
       target: post.title,
       dispatcherId: post.authorId,
       occurredAt,
---
+++ src/infra/events/on-post-created.e2e-spec.ts
@@ -0,0 +1,61 @@
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@/app.module';
+import { DatabaseModule } from '../db/database.module';
+import supertest from 'supertest';
+import { UserFactory } from 'test/factories/user-factory';
+import { TokenPayload } from '../auth/jwt-strategy';
+import { JwtService } from '@nestjs/jwt';
+import { CreatePostDto } from '../http/dtos/create-post';
+import { PrismaService } from '../db/prisma/prisma-service';
+import { waitFor } from 'test/utlils/wait-for';
+
+describe('On Post Created Event handler', () => {
+  let app: INestApplication;
+  let jwt: JwtService;
+  let prisma: PrismaService;
+  let userFactory: UserFactory;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory],
+    }).compile();
+
+    app = module.createNestApplication();
+    jwt = module.get(JwtService);
+    prisma = module.get(PrismaService);
+    userFactory = module.get(UserFactory);
+
+    await app.init();
+  });
+
+  it('should register a new log when a post is created', async () => {
+    const user = await userFactory.createAndPersist('admin');
+
+    const token = await jwt.signAsync({
+      name: user.name,
+      role: user.role,
+      sub: user.id.toValue(),
+    } as TokenPayload);
+
+    const postTitle = 'Testando o evento de criação de post!';
+
+    const _response = await supertest(app.getHttpServer())
+      .post('/post/new')
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        content: 'Conteúdo do meu primeiro post fictício!',
+        tags: ['eventos', 'domínio'],
+        title: postTitle,
+        topstory: 'https://i.imgur.com/NQ9ImcM.png',
+        authorId: user.id.toValue(),
+      } as CreatePostDto);
+
+    waitFor(async () => {
+      const logsOnDb = await prisma.log.findMany();
+
+      expect(logsOnDb.length).toBe(1);
+    }, 5000);
+  });
+});
