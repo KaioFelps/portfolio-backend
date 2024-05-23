44e56b4df022704c384d4edf24f685d52d9eef66
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-23 20:50:19 -0300

    build: listen server on env set port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { EnvService } from './infra/env/env-service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
 
-  await app.listen(3000);
+  const port = app.get(EnvService).get('PORT');
+  await app.listen(port);
 }
 bootstrap();
