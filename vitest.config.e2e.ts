64b26e71481ba2fde3e71e403764187db35de774
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-23 17:07:43 -0300

    chore: increase test timeout limit

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     root: './',
     // run this file before the tests begin
     setupFiles: ['./test/test-e2e.ts'],
-    testTimeout: 20000,
+    testTimeout: 40000,
   },
   plugins: [
     // This is required to build the test files with SWC
