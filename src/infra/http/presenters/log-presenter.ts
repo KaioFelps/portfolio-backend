1177631614400bf2aacf86888e53ff3d6ffca2b4
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-22 23:18:45 -0300

    fix: log presenter

--- src/infra/http/presenters/log-presenter.ts
+++ src/infra/http/presenters/log-presenter.ts
@@ -1,11 +1,12 @@
 import { LogWithAuthor } from '@/domain/logs/entities/value-objects/log-with-author';
+import { UserPresenter } from './user-presenter';
 
 export class LogPresenter {
   static toHTTP(log: LogWithAuthor) {
     return {
-      id: log.id,
+      id: log.id.toValue(),
       action: log.action,
-      author: log.dispatcher,
+      author: log.dispatcher ? UserPresenter.toHTTP(log.dispatcher) : null,
       target: log.target,
       targetType: log.targetType,
       createdAt: log.createdAt,
