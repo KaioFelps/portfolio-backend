86e75979701e2f3e7edcd407254c3097ab889180
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-23 02:31:47 -0300

    test: on post edited

---
+++ src/infra/events/on-post-edited.e2e-spec.ts
@@ -0,0 +1,61 @@
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@/app.module';
+import { DatabaseModule } from '../db/database.module';
+import supertest from 'supertest';
+import { UserFactory } from 'test/factories/user-factory';
+import { TokenPayload } from '../auth/jwt-strategy';
+import { JwtService } from '@nestjs/jwt';
+import { CreatePostDto } from '../http/dtos/create-post';
+import { PrismaService } from '../db/prisma/prisma-service';
+import { waitFor } from 'test/utlils/wait-for';
+import { PostFactory } from 'test/factories/post-factory';
+
+describe('On Post Edited Event handler', () => {
+  let app: INestApplication;
+  let jwt: JwtService;
+  let prisma: PrismaService;
+  let userFactory: UserFactory;
+  let postFactory: PostFactory;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory, PostFactory],
+    }).compile();
+
+    app = module.createNestApplication();
+    jwt = module.get(JwtService);
+    prisma = module.get(PrismaService);
+    userFactory = module.get(UserFactory);
+    postFactory = module.get(PostFactory);
+    await app.init();
+  });
+
+  it('should register a new log when a post is edited', async () => {
+    const user = await userFactory.createAndPersist('admin');
+    const post = await postFactory.createAndPersist({ authorId: user.id });
+
+    const token = await jwt.signAsync({
+      name: user.name,
+      role: user.role,
+      sub: user.id.toValue(),
+    } as TokenPayload);
+
+    const newPostTitle = 'Título editadooo!!';
+
+    const _response = await supertest(app.getHttpServer())
+      .put(`/post/${post.id.toValue()}/edit`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        tags: ['eventos'],
+        title: newPostTitle,
+      } as CreatePostDto);
+
+    await waitFor(async () => {
+      const logsOnDb = await prisma.log.findMany();
+
+      expect(logsOnDb.length).toBe(1);
+    }, 10000);
+  });
+});
