0a93d70849b915df505d82117793d67632c47466
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-05-22 23:19:15 -0300

    chore: log factory - fix exec method - add createAndPersist method

--- test/factories/log-factory.ts
+++ test/factories/log-factory.ts
@@ -1,22 +1,45 @@
-import { EntityUniqueId } from '@/core/entities/entity-unique-id';
 import { Optional } from '@/core/types/optional';
-import { Log, LogAction, LogProps } from '@/domain/logs/entities/log';
+import {
+  Log,
+  LogAction,
+  LogProps,
+  LogTargetType,
+} from '@/domain/logs/entities/log';
+import { PrismaLogMapper } from '@/infra/db/prisma/mappers/prisma-log-mapper';
+import { PrismaService } from '@/infra/db/prisma/prisma-service';
 import { faker } from '@faker-js/faker';
-import { randomUUID } from 'crypto';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class LogFactory {
+  constructor(private prisma: PrismaService) {}
+
   static exec(
     override?: Optional<
       LogProps,
-      'action' | 'createdAt' | 'dispatcherId' | 'target'
+      'action' | 'createdAt' | 'dispatcherId' | 'target' | 'targetType'
     >,
   ) {
     return Log.create({
       action: LogAction.created,
       target: faker.lorem.sentence(),
-      dispatcherId: new EntityUniqueId(randomUUID()),
+      dispatcherId: null,
       createdAt: new Date(),
+      targetType: LogTargetType.post,
       ...override,
     });
+  }
+
+  public async createAndPersist(
+    override?: Optional<
+      LogProps,
+      'action' | 'createdAt' | 'dispatcherId' | 'target'
+    >,
+  ) {
+    const log = LogFactory.exec(override);
+
+    await this.prisma.log.create(PrismaLogMapper.toPrisma(log));
+
+    return log;
   }
 }
