6029b722a52a70208cd89020aae236132012b72f
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-06-04 17:09:22 -0300

    build: raise a root user with env variables credentials on app bootstrap if there is not any user with that email so far

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { EnvService } from './infra/env/env-service';
+import { IHashGenerator } from './core/crypt/hash-generator';
+import { PrismaService } from './infra/db/prisma/prisma-service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
 
-  const port = app.get(EnvService).get('PORT');
+  const env = app.get(EnvService);
+  const port = env.get('PORT');
+
+  const password = await app
+    .get(IHashGenerator)
+    .generate(env.get('ROOTUSER_PASSWORD'));
+
+  await app.get(PrismaService).user.upsert({
+    create: {
+      email: env.get('ROOTUSER_EMAIL'),
+      name: env.get('ROOTUSER_NAME'),
+      password,
+      role: 'ADMIN',
+    },
+    update: {},
+    where: { email: env.get('ROOTUSER_EMAIL') },
+  });
+
   await app.listen(port);
 }
+
 bootstrap();
