40cb8a72c164520d6185c04235e547dae7a3e53b
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-07-23 22:40:33 -0300

    feat: tag controller implement missing methods

--- src/infra/http/controllers/tag.controller.e2e-spec.ts
+++ src/infra/http/controllers/tag.controller.e2e-spec.ts
@@ -6,20 +6,27 @@ import { INestApplication } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserFactory } from 'test/factories/user-factory';
+import { TagFactory } from 'test/factories/tag-factory';
+import { QUANTITY_PER_PAGE } from '@/core/pagination-consts';
+import { PrismaService } from '@/infra/db/prisma/prisma-service';
 
 describe('TagController', () => {
   let app: INestApplication;
   let userFactory: UserFactory;
+  let tagFactory: TagFactory;
+  let prisma: PrismaService;
   let jwt: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [UserFactory, JwtModule],
+      providers: [UserFactory, JwtModule, TagFactory, PrismaService],
     }).compile();
 
     app = module.createNestApplication();
     userFactory = module.get(UserFactory);
+    tagFactory = module.get(TagFactory);
+    prisma = module.get(PrismaService);
     jwt = module.get(JwtService);
 
     await app.init();
@@ -47,5 +54,75 @@ describe('TagController', () => {
         value: 'Foo',
       }),
     });
+  });
+
+  test('[GET] /tag/list', async () => {
+    for (let i = 0; i <= 14; i++) {
+      await tagFactory.createAndPersist();
+    }
+
+    const response = await supertest(app.getHttpServer())
+      .get('/tag/list?page=2')
+      .send()
+      .expect(200);
+
+    expect(response.body).toMatchObject({
+      tags: expect.any(Array),
+      totalCount: 15,
+      page: 2,
+      perPage: QUANTITY_PER_PAGE,
+    });
+
+    expect(response.body.tags.length).toBe(3);
+  });
+
+  test('[PATCH] /tag/:id/edit', async () => {
+    const user = await userFactory.createAndPersist('admin');
+    const token = await jwt.signAsync({
+      name: user.name,
+      role: user.role,
+      sub: user.id.toValue(),
+    });
+
+    const tag = await tagFactory.createAndPersist({ value: 'front end' });
+
+    const response = await supertest(app.getHttpServer())
+      .patch(`/tag/${tag.id.toValue()}/edit`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        value: 'rust',
+      })
+      .expect(200);
+
+    const tagOnDb = await prisma.tag.findUnique({
+      where: { id: tag.id.toValue() },
+    });
+
+    expect(tagOnDb!.value).toEqual('rust');
+    expect(response.body.tag).toMatchObject({
+      id: tag.id.toValue(),
+      value: 'rust',
+    });
+  });
+
+  test('[DELETE] /tag/:id/delete', async () => {
+    const user = await userFactory.createAndPersist('admin');
+    const token = await jwt.signAsync({
+      name: user.name,
+      role: user.role,
+      sub: user.id.toValue(),
+    });
+
+    const tag = await tagFactory.createAndPersist();
+
+    await supertest(app.getHttpServer())
+      .delete(`/tag/${tag.id.toValue()}/delete`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send()
+      .expect(204);
+
+    const tagOnDb = await prisma.tag.findMany();
+
+    expect(tagOnDb.length).toBe(0);
   });
 });
--- src/infra/http/controllers/tag.controller.ts
+++ src/infra/http/controllers/tag.controller.ts
@@ -6,14 +6,36 @@ import {
   HttpCode,
   InternalServerErrorException,
   Post,
+  Get,
+  Patch,
+  Delete,
+  Query,
+  Param,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { TagPresenter } from '../presenters/tag-presenter';
 import { CreateTagDto } from '../dtos/create-tag';
+import { FetchManyTagsService } from '@/domain/tags/services/fetch-many-tags-service';
+import { PaginatedQueryDto } from '../dtos/paginated-query';
+import { QUANTITY_PER_PAGE } from '@/core/pagination-consts';
+import { UpdateTagDto } from '../dtos/update-tag';
+import { TokenPayload } from '@/infra/auth/jwt-strategy';
+import { CurrentUser } from '@/infra/auth/decorators/current-user';
+import { PublicRoute } from '@/infra/auth/decorators/public-route';
+import { EditTagService } from '@/domain/tags/services/edit-tag-service';
+import { DeleteTagService } from '@/domain/tags/services/delete-tag-service';
+import { UnauthorizedError } from '@/core/errors/unauthorized-error';
+import { BadRequestError } from '@/core/errors/bad-request-error';
 
 @Controller('/tag')
 export class TagsController {
-  constructor(private createTagService: CreateTagService) {}
+  constructor(
+    private createTagService: CreateTagService,
+    private fetchManyTagsService: FetchManyTagsService,
+    private editTagService: EditTagService,
+    private deleteTagService: DeleteTagService,
+  ) {}
 
   @Post('/new')
   @HttpCode(201)
@@ -32,6 +54,65 @@ export class TagsController {
       }
 
       throw new InternalServerErrorException();
+    }
+  }
+
+  @Get('/list')
+  @HttpCode(200)
+  @PublicRoute()
+  async list(@Query() query: PaginatedQueryDto) {
+    const result = await this.fetchManyTagsService.exec(query);
+
+    if (result.isFail()) {
+      throw new InternalServerErrorException();
+    }
+
+    const formattedTags = result.value.tags.map(TagPresenter.toHTTP);
+
+    return {
+      tags: formattedTags,
+      totalCount: result.value.count,
+      page: query.page || 1,
+      perPage: query.amount || QUANTITY_PER_PAGE,
+    };
+  }
+
+  @Patch('/:id/edit')
+  @HttpCode(200)
+  async update(
+    @Body() body: UpdateTagDto,
+    @CurrentUser() user: TokenPayload,
+    @Param('id') tagId: string,
+  ) {
+    const result = await this.editTagService.exec({
+      tagId,
+      value: body.value,
+    });
+
+    if (result.isFail()) {
+      switch (result.value.constructor) {
+        case UnauthorizedError:
+          throw new UnauthorizedException();
+        case BadRequestError:
+          throw new BadRequestException();
+        default:
+          throw new BadRequestError();
+      }
+    }
+
+    return { tag: TagPresenter.toHTTP(result.value.tag) };
+  }
+
+  @Delete('/:id/delete')
+  @HttpCode(204)
+  async delete(@CurrentUser() user: TokenPayload, @Param('id') tagId: string) {
+    const result = await this.deleteTagService.exec({
+      userId: user.sub,
+      tagId,
+    });
+
+    if (result.isFail()) {
+      throw new UnauthorizedException();
     }
   }
 }
