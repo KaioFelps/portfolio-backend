4a88f003f11311da3d5b2f4a1916ce41b9c03784
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-30 03:57:21 -0300

    fix(prisma adapter factory): omit schema options if no schema has been provided

--- src/infra/db/prisma/create-adapter.ts
+++ src/infra/db/prisma/create-adapter.ts
@@ -1,9 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 
 export function createPgAdapter(connString: string) {
-  const schema = new URL(connString).searchParams.get("schema")!;
-  return new PrismaPg(
-    { connectionString: process.env["DATABASE_URL"], options: `-c search_path="${schema}"` },
-    { schema },
-  );
+  const schema = new URL(connString).searchParams.get("schema") ?? undefined;
+  const options = schema ? `-c search_path="${schema}"` : undefined;
+  return new PrismaPg({ connectionString: process.env["DATABASE_URL"], options }, { schema });
 }
